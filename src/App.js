8b28b241a085b01762dabee9ab47613bfbabc963
parag28 <parag@Parags-MacBook-Air.local>
2023-09-03 21:11:12 -0400

    improved dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const[mode, setMode] = useState('light');
+
+  const toggleMode =()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey';
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-    <Navbar title="Change Case"/>
+    <Navbar title="Change Case" mode={mode} toggleMode={toggleMode}/>
     <div className='container my-3'>
-      {/* <TextForm heading = "Enter Text below"/> */}
-      <About/>
+      <TextForm heading = "Enter Text below" mode={mode}/>
+      {/* <About/> */}
     </div>
     </>
    
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,6 +12,7 @@ export default function TextForm(props) {
         setText(newText)
     }
     const handleOnChange = (event)=>{
+        var text=document.getElementById("myBox")
         setText(event.target.value)
     }
     const toTitleCase = (event)=>{
@@ -25,16 +26,16 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}} >
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="7"></textarea>
+            <textarea className="form-control" id="myBox" value={text} style={{backgtoundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} rows="7"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleOnUpClick}>Convert to upperCase</button>
         <button className="btn btn-primary mx-2" onClick={handleOnLowClick}>Convert to LowerCase</button>
         <button className="btn btn-primary mx-2" onClick={toTitleCase}>Convert to TitleCase</button>
     </div>
-    <div className="container">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>Your Text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
